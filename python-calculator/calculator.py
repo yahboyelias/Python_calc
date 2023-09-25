da24ad8718d4792684c2265116695d695a4d731d
Elias <54425552+yahboyelias@users.noreply.github.com>
2023-09-24 21:51:41 -0400

    Update calculator.py

--- python-calculator/calculator.py
+++ python-calculator/calculator.py
@@ -1,54 +1,57 @@
+# Import the math module to access mathematical functions
 import math
 
+# Define a function for adding two numbers
 def add(x, y):
-    """Add two numbers."""
     return x + y
 
+# Define a function for subtracting one number from another
 def subtract(x, y):
-    """Subtract y from x."""
     return x - y
 
+# Define a function for multiplying two numbers
 def multiply(x, y):
-    """Multiply two numbers."""
     return x * y
 
+# Define a function for dividing one number by another, with handling for division by zero
 def divide(x, y):
-    """Divide x by y, handling division by zero."""
     if y == 0:
         return "Cannot divide by zero"
     return x / y
 
+# Define a function for calculating the square root of a number
 def square_root(x):
-    """Calculate the square root of x."""
     if x < 0:
         return "Invalid input"
     return math.sqrt(x)
 
+# Define a function for calculating the result of raising one number to the power of another
 def power(x, y):
-    """Calculate x raised to the power of y."""
     return x ** y
 
+# Define a function for calculating the factorial of a number
 def factorial(x):
-    """Calculate the factorial of x."""
     if x < 0:
         return "Invalid input"
     if x == 0:
         return 1
     return math.factorial(x)
 
+# Define a function for calculating the average of a list of numbers
 def calculate_average(numbers):
-    """Calculate the average of a list of numbers."""
     if len(numbers) == 0:
         return "List is empty"
     return sum(numbers) / len(numbers)
 
+# Define a function for calculating the percentage of one number relative to another
 def calculate_percentage(x, y):
-    """Calculate the percentage of y relative to x."""
     if y == 0:
         return "Cannot calculate percentage with denominator zero"
     return (x / y) * 100
 
+# Create an infinite loop to keep the program running until the user chooses to quit
 while True:
+    # Display a list of available options to the user
     print("Options:")
     print("Enter 'add' for addition")
     print("Enter 'subtract' for subtraction")
@@ -61,50 +64,69 @@ while True:
     print("Enter 'percentage' for calculating percentage")
     print("Enter 'quit' to end the program")
 
+    # Prompt the user for their choice
     user_input = input(": ")
 
+    # Check if the user wants to quit the program
     if user_input == "quit":
         break
+    # Check if the user's input matches one of the available mathematical operations
     elif user_input in ("add", "subtract", "multiply", "divide", "sqrt", "power", "factorial"):
-        if user_input in ("sqrt", "factorial"):
-            num = float(input("Enter a number: "))
-            if user_input == "sqrt":
-                print("Result: " + str(square_root(num)))
-            else:
-                print("Result: " + str(factorial(int(num))))
-        else:
+        try:
+            # Prompt the user for input numbers
             num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-
-            if user_input == "add":
-                print("Result: " + str(add(num1, num2)))
-            elif user_input == "subtract":
-                print("Result: " + str(subtract(num1, num2)))
-            elif user_input == "multiply":
-                print("Result: " + str(multiply(num1, num2)))
-            elif user_input == "divide":
-                print("Result: " + str(divide(num1, num2)))
-            elif user_input == "power":
-                print("Result: " + str(power(num1, num2)))
+            # Check if the selected operation is square root or factorial
+            if user_input in ("sqrt", "factorial"):
+                if user_input == "sqrt":
+                    # Calculate and print the result of the square root operation
+                    print("Result:", square_root(num1))
+                else:
+                    # Calculate and print the result of the factorial operation
+                    print("Result:", factorial(int(num1)))
+            else:
+                # For other operations (add, subtract, multiply, divide, power), prompt for a second number
+                num2 = float(input("Enter second number: "))
+                # Perform the selected operation and print the result
+                if user_input == "add":
+                    print("Result:", add(num1, num2))
+                elif user_input == "subtract":
+                    print("Result:", subtract(num1, num2))
+                elif user_input == "multiply":
+                    print("Result:", multiply(num1, num2))
+                elif user_input == "divide":
+                    print("Result:", divide(num1, num2))
+                elif user_input == "power":
+                    print("Result:", power(num1, num2))
+        except ValueError:
+            # Handle invalid input by displaying an error message
+            print("Invalid input. Please enter valid numbers.")
+    # Check if the user's input matches the average or percentage operations
     elif user_input in ("average", "percentage"):
         num_list = []
         while True:
+            # Prompt the user for a series of numbers or "done" to finish
             num = input("Enter a number (or 'done' to finish): ")
             if num.lower() == 'done':
                 break
             try:
+                # Convert the input to a float and append it to the list
                 num_list.append(float(num))
             except ValueError:
+                # Handle invalid input by displaying an error message
                 print("Invalid input. Please enter a valid number.")
         
         if user_input == "average":
-            print("Result: " + str(calculate_average(num_list)))
+            # Calculate and print the average of the entered numbers
+            print("Result:", calculate_average(num_list))
         elif user_input == "percentage":
+            # Check if there are at least two numbers to calculate the percentage
             if len(num_list) < 2:
                 print("At least two numbers are required to calculate a percentage.")
             else:
+                # Calculate and print the percentage
                 x = num_list[0]
                 y = num_list[1]
-                print("Result: " + str(calculate_percentage(x, y)))
+                print("Result:", calculate_percentage(x, y))
     else:
+        # Handle invalid input by displaying an error message
         print("Invalid input")
